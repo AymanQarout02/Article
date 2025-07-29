b2eaaa70dc8ac1d7f85b080b682056f38bb27000
ayman <aymanwkm2002@gmail.com>
2025-07-29 11:01:31 +0300

    Create Migrations

---
+++ database/migrations/2025_07_28_144408_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('path')->unique();
+            $table->string('type')->nullable();
+            $table->string('size')->nullable();
+            $table->string('extension')->nullable();
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
